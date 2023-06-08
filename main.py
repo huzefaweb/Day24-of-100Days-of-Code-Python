0e8745d79516e1384cab1a8f6a372fad87012e24
Huzefa Ahmed <106472690+huzefaweb@users.noreply.github.com>
2023-06-08 14:49:38 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+PLACEHOLDER = "name"
+with open("./Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    content = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = content.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as invitations:
+            invitations.write(new_letter)
